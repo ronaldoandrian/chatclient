9d4035790f3f2f1eaddd9b042a0b4192850fc59e
Ronaldo Andrianandrasana Rafaramalala (BSN-STG) <Ronaldo.Rafaramalala@basan.mg>
2019-09-27 15:12:36 +0300

    add component login and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './authentification/login/login.component';
+import { RegisterComponent } from './authentification/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
